695f4081d5caa4dc905458d19387dbeb781bc528
KauanRakoski <kauan.k.rakoski@gmail.com>
2021-02-06 02:36:32 +0000

    new styling

--- client/src/views/Subscription.vue
+++ client/src/views/Subscription.vue
@@ -1,25 +1,33 @@
 <template>
-  <div class="main">
-    <form id="payment-form" class="payment shadow">
-      <h2 class="subscribe-text">{{ msg }} {{ payAmount }}</h2>
-      <div ref="card" class="form-control"></div>
-      <input
-        :disabled="lockSubmit"
-        class="button big btn-main shadow-sm"
-        type="submit"
-        value="Subscribe"
-        v-on:click.prevent="purchase"
-      />
-    </form>
+  <div>
+    <Header :img="getUser().photoURL" :mode="'white'" />
+    <div class="main">
+      <form id="payment-form" class="payment shadow">
+        <h3 class="subscribe-text">Subscribe for just $0,50</h3>
+        <div ref="card" class="form-control"></div>
+        <input
+          :disabled="lockSubmit"
+          class="button big btn-main shadow-sm"
+          type="submit"
+          value="Subscribe"
+          v-on:click.prevent="purchase"
+        />
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
+import Header from "../components/multi-pages/Header";
+/* import utilities from '../helpers/utilites' */
 
 export default {
   name: "Subscription",
+  components: {
+    Header,
+  },
   data() {
     return {
       backendUrl:
@@ -28,13 +36,16 @@ export default {
         "pk_test_51HuKA0J1YBMak5vmu4kTKH0t32DNFWrkjaZIe2Tzcu6KF2lqzro7KJp6kA9G29wkCGWafrlILXAweAZHuFJdUxg600WqPMhPpa",
       stripe: undefined,
       card: undefined,
-      msg: "Subscribe",
-      payAmount: "$0.50",
       lockSubmit: false,
       api: "https://yourdomain.com/api/",
     };
   },
   methods: {
+    getUser() {
+      let userInfo = JSON.parse(localStorage.getItem("AuthUser"));
+
+      return userInfo;
+    },
     async purchase() {
       this.lockSubmit = true;
 
@@ -57,13 +68,20 @@ export default {
           email: userEmail,
         });
 
-        const status  = res.data.status;
+        const status = res.data.status;
         console.log(res.data);
 
         if (status == "active") this.$router.push("/dashboard");
       }
     },
   },
+  /* beforeMount(){
+    let userInfo = JSON.parse(localStorage.getItem("AuthUser"));
+    let userEmail = userInfo.email;
+
+    let subscription = utilities.checkUserSubscription(userEmail).then(sub => {return sub})
+    if(subscription != undefined) this.$router.push('/dashboard') 
+  }, */
   async mounted() {
     this.stripe = await loadStripe(this.spk);
     this.card = this.stripe.elements().create("card");
@@ -78,6 +96,8 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+  height: 83vh;
+  justify-content: center;
 }
 .subscribe-text {
   font-family: "Inter", sans-serif;
@@ -85,7 +105,7 @@ export default {
 }
 .payment {
   width: 400px;
-
+  border-radius: 4px;
   padding: 3em 1em;
   display: flex;
   flex-direction: column;
