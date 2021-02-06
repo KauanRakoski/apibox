3b91641e4e38baa0c381f771af1ce79eed53a42b
KauanRakoski <kauan.k.rakoski@gmail.com>
2021-02-06 02:37:43 +0000

    better component organization

---
+++ client/src/components/multi-pages/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer>
+    <div>
+      <img src="../../assets/controlLogo.svg" width="30px" height="30px" />
+    </div>
+
+    <div>
+        <p><a href="#">Documentation</a></p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  width: 100%;
+  border-top: 1px solid rgb(178, 178, 178);
+  z-index: 3;
+  position: absolute;
+  bottom: 0;
+  
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1em;
+}
+footer p a{
+    color: black;
+    text-decoration: none;
+    font-family: "Inter", sans-serif;
+    font-weight: 500;
+    font-size: 16px;
+}
+footer p a:hover{
+    color: rgb(52, 52, 52);
+}
+</style>
