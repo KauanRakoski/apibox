16c52bcf46cb509c0b988b6a07b40ddf8d980c42
KauanRakoski <kauan.k.rakoski@gmail.com>
2021-02-16 17:29:02 +0000

    pwa icons and manifest

--- client/src/main.js
+++ client/src/main.js
@@ -6,7 +6,6 @@ import router from './router'
 import PerfectScrollbar from 'vue2-perfect-scrollbar'
 import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css'
 
-
 Vue.use(PerfectScrollbar)
 
 Vue.config.productionTip = false
--- client/vue.config.js
+++ client/vue.config.js
@@ -3,4 +3,46 @@ module.exports = {
         disableHostCheck: true,
         host: '0.0.0.0'
     },
+    pwa: {
+    "manifest_version": 2,
+    "version": "0",
+    "name": "jsint",
+    "short_name": "jsint",
+    "lang": "en-US",
+    "start_url": "/",
+    "display": "standalone",
+    "theme_color": "#4460F1",
+    "background_color": "#FFFFFF",
+    "icons": [{
+        "src": "public/icons/homescreen48.png",
+        "sizes": "48x48",
+        "type": "image/png"
+    }, {
+        "src": "/icons/homescreen72.png",
+        "sizes": "72x72",
+        "type": "image/png"
+    }, {
+        "src": "/icons/homescreen96.png",
+        "sizes": "96x96",
+        "type": "image/png"
+    }, {
+        "src": "/icons/homescreen144.png",
+        "sizes": "144x144",
+        "type": "image/png"
+    }, {
+        "src": "/icons/homescreen168.png",
+        "sizes": "168x168",
+        "type": "image/png"
+    }, {
+        "src": "/icons/homescreen192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+    },
+    {
+        "src": "public/icons/homescreen512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+    }
+]
+    }
 }
