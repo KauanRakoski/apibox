f1bd39afae73559a6621399267844639e025ecb2
KauanRakoski <kauan.k.rakoski@gmail.com>
2021-05-24 16:48:05 +0000

    Run function

--- client/src/views/New.vue
+++ client/src/views/New.vue
@@ -30,19 +30,26 @@
 
         <div class="i-group">
           <label for="key">Select a key: </label>
-          <select required name="key" id="key">
+          
+          <select required name="key" id="key" class="form-select pr-1">
             <option value="slider">Slider</option>
             <option value="click">Click</option>
           </select>
+          
         </div>
 
         <div class="i-group">
           <label for="code">Action:</label>
-          <div class="before"><img src="https://i.ibb.co/wKm3dwr/settings.png">JavaScript</div>
+          <div class="before">
+            <img src="https://i.ibb.co/wKm3dwr/settings.png" />JavaScript
+          </div>
           <codemirror class="editor" v-model="code" :options="cmOptions" />
           <div class="after">
-              <p>Sandbox</p>
-              <a><img src="https://i.ibb.co/W29t3kB/play-button-arrowhead.png"> Run</a>
+            <p>Sandbox</p>
+            <a @click="runTest()"
+              ><img src="https://i.ibb.co/W29t3kB/play-button-arrowhead.png" />
+              Run</a
+            >
           </div>
         </div>
 
@@ -60,19 +67,18 @@
 <script>
 import Header from "../components/multi-pages/Header";
 import axios from "axios";
-/* import swal from 'sweetalert' */
+import Swal from "sweetalert2";
 import utilities from "../helpers/utilities";
-import firebase from 'firebase';
-import { codemirror } from 'vue-codemirror'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/mode/javascript/javascript.js'
-import 'codemirror/theme/dracula.css'
+import { codemirror } from "vue-codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/mode/javascript/javascript.js";
+import "codemirror/theme/dracula.css";
 
 export default {
   name: "New",
   components: {
     Header,
-    codemirror
+    codemirror,
   },
   data() {
     return {
@@ -80,130 +86,136 @@ export default {
       code: 'console.log("Hello World")',
       cmOptions: {
         tabSize: 4,
-        mode: 'text/javascript',
-        theme: 'dracula',
+        mode: "text/javascript",
+        theme: "dracula",
         lineNumbers: true,
         line: true,
-      }
+      },
     };
   },
   methods: {
     getUser() {
-      let userInfo = localStorage.getItem("AuthUser");
+      let userInfo = localStorage.getItem("apibox-user");
       let jsonInfo = JSON.parse(userInfo);
-      
+
       return jsonInfo;
     },
     async submit() {
-        let name = document.getElementById("name").value;
-        let description = document.getElementById("description").value;
-        let key = document.getElementById("key").value;
-        
-        try {
-            if (!this.$route.params.task) {
-            await axios.post(`${this.postRoute}/add/${this.getUser().uid}`, {
-                name: name,
-                key: key,
-                description: description,
-                action: this.code
-            });
-            console.log("hello")
-            } else {
-            await axios.post(
-                `${this.postRoute}/edit/${this.$route.params.task._id}`,
-                { name: name, key: key, description: description, action: this.code }
-            );
-            console.log("hello")
-            }
-        }catch(e){
-            utilities.showError(e)
-        }
+      let name = document.getElementById("name").value;
+      let description = document.getElementById("description").value;
+      let key = document.getElementById("key").value;
 
-        this.$router.push("/dashboard");
+      try {
+        if (!this.$route.params.task) {
+          await axios.post(`${this.postRoute}/add/${this.getUser().uid}`, {
+            name: name,
+            key: key,
+            description: description,
+            action: this.code,
+          });
+          console.log("hello");
+        } else {
+          await axios.post(
+            `${this.postRoute}/edit/${this.$route.params.task._id}`,
+            {
+              name: name,
+              key: key,
+              description: description,
+              action: this.code,
+            }
+          );
+          console.log("hello");
+        }
+      } catch (e) {
+        utilities.showError(e);
+      }
+
+      this.$router.push("/dashboard");
     },
+    async runTest() {
+      var res = await axios.post(`${this.postRoute}/api/sandbox`, {
+        code: this.code,
+      });
+
+      Swal.fire({
+        title: `${res.data.details}`,
+        icon: "info",
+        text: `Response: ${res.data.response}`,
+      });
+      console.log(res.data);
     },
-    beforeMount(){
-        if(firebase.auth().currentUser == null || firebase.auth().currentUser == undefined){
-        let userInfo = localStorage.getItem("AuthUser")
-        var jsonInfo = JSON.parse(userInfo)
-        } else jsonInfo = firebase.auth().currentUser
-        
-        utilities.checkUserSubscription(jsonInfo.email)
-        .then(subscription => {
-        if(subscription == undefined) this.$router.push('/subscribe')
-        this.costumerId = subscription.id
-        })
-        .catch(e => utilities.showError(e))
-    },
+  },
   mounted() {
-      
-      if (this.$route.params.task != undefined) {
+    if (this.$route.params.task != undefined) {
       document.getElementById("name").value = this.$route.params.task.name;
       document.getElementById(
         "description"
       ).value = this.$route.params.task.description;
       document.getElementById("key").value = this.$route.params.task.key;
 
-      this.code = this.$route.params.task.action
+      this.code = this.$route.params.task.action;
     }
-
-    
   },
 };
 </script>
 
 <style scoped>
-@import url('https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css');
+@import url("https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css");
 
 .vue-codemirror {
-    width: 400px;
+  width: 400px;
 }
-.before{
-    display: flex;
-    align-items: center;
-    font-weight: 500;
-    border-top-left-radius: 5px;
-    border-top-right-radius: 5px;
-    width: 100%;
-    color: white;
-    background-color: #282a36;
-    padding: 0.4em 1em;
+.before {
+  display: flex;
+  align-items: center;
+  font-weight: 500;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  width: 100%;
+  color: white;
+  background-color: #282a36;
+  padding: 0.4em 1em;
 }
-.before img{
-    width: 16px;
-    margin-right: 10px;
+.before img {
+  width: 16px;
+  margin-right: 10px;
 }
-.after{
-    width: 100%;
-    background-color: #383a49;
-    height: 40px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom-right-radius: 5px;
-    border-bottom-left-radius: 5px;
+.after {
+  width: 100%;
+  background-color: #383a49;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom-right-radius: 5px;
+  border-bottom-left-radius: 5px;
 }
-.after img{
-    transform: scale(0.9)
+.after img {
+  transform: scale(0.9);
 }
-.after p{
-    color: white;
-    margin: 0px 0 0 20px;
-    font-family: "Inter", sans-serif;
-    font-weight: bold;
+.after p {
+  color: white;
+  margin: 0px 0 0 20px;
+  font-family: "Inter", sans-serif;
+  font-weight: bold;
 }
-.after a{
-    border-bottom-right-radius: 5px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    cursor: pointer;
-    color: white;
-    height: 100%;
-    width: 80px;
-    padding: 5px;
-    background: #33b277;
-    border: none;
+.after a {
+  border-bottom-right-radius: 5px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  cursor: pointer;
+  color: white;
+  height: 100%;
+  width: 80px;
+  padding: 5px;
+  background-color: #33b277;
+  border: none;
+}
+.after a:hover {
+  text-decoration: none;
+  color: white;
+  background-color: #30a56f;
 }
 body {
   overflow-x: hidden;
@@ -264,23 +276,10 @@ textarea {
   background-color: rgb(236, 228, 228);
   min-height: 100px;
 }
-select {
-  padding: 0.5em 2em;
-  border-radius: 3px;
-  border: 1px solid rgb(199, 199, 199);
-  cursor: pointer;
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  appearance: none;
-}
-select option {
-  font-family: "Inter", sans-serif;
-  border-radius: 40px;
-  font-size: 15px;
-  font-weight: 300;
-  border-radius: 88px;
-  cursor: pointer;
-}
+/* select{
+    padding: 0.3em 0.8em;
+    border-radius: 3px;
+} */
 @media only screen and (max-width: 490px) {
   form {
     width: 360px;
